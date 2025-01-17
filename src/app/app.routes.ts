efdd7392d64b8636b0eba1c610fd923362982e8c
ROMG <ironrafa96hotmail.com>
2025-01-17 13:50:26 -0600

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ProductComponent } from './components/product/product.component';
+import { ProductDetailComponent } from './components/product-detail/product-detail.component';
+import { ContactComponent } from './components/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'products', component: ProductComponent},
+    {path: 'products/:id', component: ProductDetailComponent},
+    {path: 'contact', component: ContactComponent},
+    {path: '**', redirectTo: '', pathMatch: 'full'},
+];
